09e816eaa98f80f7d8487eedf07d07e9e642362e
nyyctx <lucas_nunes_ribeiro@hotmail.com>
2023-02-18 03:20:37 -0300

    Good practices for html update

--- quickdm/quickmenu.js
+++ quickdm/quickmenu.js
@@ -1,4 +1,8 @@
-const toggleBtn = document.querySelector(".toggle-button");
+const toggleBtn = document.querySelectorAll(".toggle-button");
 const dashboardMenu = document.querySelector("#menu-container");
 
-toggleBtn.addEventListener("click", function () { dashboardMenu.classList.toggle("open"); });
+toggleBtn.forEach((button) => {
+  button.addEventListener("click", function () {
+    dashboardMenu.classList.toggle("open");
+  });
+});
